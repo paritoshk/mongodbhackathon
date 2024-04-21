726c69b2b1ed2cd0e0eba146e28389520795013c
KA WAI Tsoi <edtsoi430@gmail.com>
2024-04-20 19:15:59 -0700

    change LLM

--- vectorsearch.py
+++ vectorsearch.py
@@ -6,6 +6,9 @@ import os
 from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, StorageContext, Settings
 from llama_index.vector_stores.mongodb import MongoDBAtlasVectorSearch
 from llama_index.llms.mistralai import MistralAI
+from fireworks.client import Fireworks
+from llama_index.llms.openai import OpenAI
+
 from transformers import AutoTokenizer
 
 def get_mongo_client(mongo_uri):
@@ -40,13 +43,22 @@ def query(text):
   embed_model = OpenAIEmbedding(
               model = "thenlper/gte-large",
               api_base="https://api.fireworks.ai/inference/v1",
-              api_key=os.environ["FIREWORKS_EMBED_API_KEY"],
+              api_key=os.environ["FIREWORKS_API_KEY"],
               embed_batch_size=100
           )
 
   Settings.embed_model = embed_model
 
-  llm = MistralAI(model="mistral-large-latest", temperature=0.08, api_key=os.environ["MISTRAL_API_KEY"])
+
+
+  llm = OpenAI(
+    # api_key=os.environ["FIREWORKS_API_KEY"],
+    model_name="accounts/fireworks/models/mixtral-8x7b-instruct",
+    base_url="https://api.fireworks.ai/inference/v1/completions",
+    max_tokens=256)
+  
+  # print(llm.predict(prompt="Name 3 sports that can be played by the beach."))
+  # llm = MistralAI(model="mistral-large-latest", temperature=0.08, api_key=os.environ["MISTRAL_API_KEY"])
 
   Settings.llm = llm
 
@@ -57,13 +69,13 @@ def query(text):
       index_name = "vector_index",
       embedding_key="embedding"
   )
-  # vector_store_context = StorageContext.from_defaults(vector_store=atlas_vector_search)
+  vector_store_context = StorageContext.from_defaults(vector_store=atlas_vector_search)
 
-  # vector_store_index = VectorStoreIndex.from_documents(
-  #    documents, storage_context=vector_store_context, show_progress=True
-  # )
+  vector_store_index = VectorStoreIndex.from_documents(
+     documents, storage_context=vector_store_context, show_progress=True
+  )
 
-  vector_store_index = VectorStoreIndex.from_vector_store(vector_store=atlas_vector_search)
+  # vector_store_index = VectorStoreIndex.from_vector_store(vector_store=atlas_vector_search)
 
   response = vector_store_index.as_query_engine().query(text)
   print(response)
